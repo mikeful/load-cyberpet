0893ffbf5848802ecc1dac1ff660733e8c75bb16
mikeful <mikefulli@gmail.com>
2024-01-01 17:59:39 +0200

    Added number formatting functions to 2 and 3 character sizes

--- cyberpet/toolbox.cpp
+++ cyberpet/toolbox.cpp
@@ -1,6 +1,6 @@
 #include "toolbox.h"
 
-String format_number(unsigned long amount) {
+String format_number4(unsigned long amount) {
   String parsed_amount;
 
   //if (amount > 1000000000) {
@@ -28,6 +28,62 @@ String format_number(unsigned long amount) {
     parsed_amount = "000" + String(amount);
   } else {
     parsed_amount = "0000";
+  }
+
+  return parsed_amount;
+}
+
+String format_number3(unsigned long amount) {
+  String parsed_amount;
+
+  if (amount >= 1000000000) {
+    parsed_amount = "0" + String((int)(amount / 1000000000.0)) + "B";
+  } else if (amount >= 100000000) {
+    parsed_amount = "." + String((int)(amount / 100000000.0)) + "B";
+  } else if (amount >= 10000000) {
+    parsed_amount = String((int)(amount / 1000000.0)) + "M";
+  } else if (amount >= 1000000) {
+    parsed_amount = "0" + String((int)(amount / 1000000.0)) + "M";
+  } else if (amount >= 100000) {
+    parsed_amount = "." + String((int)(amount / 100000.0)) + "M";
+  } else if (amount >= 10000) {
+    parsed_amount = String((int)(amount / 1000.0)) + "k";
+  } else if (amount >= 1000) {
+    parsed_amount = "0" + String((int)(amount / 1000.0)) + "k";
+  } else if (amount >= 100) {
+    parsed_amount = String(amount);
+  } else if (amount >= 10) {
+    parsed_amount = "0" + String(amount);
+  } else if (amount >= 1) {
+    parsed_amount = "00" + String(amount);
+  } else {
+    parsed_amount = "000";
+  }
+
+  return parsed_amount;
+}
+
+String format_number2(unsigned long amount) {
+  String parsed_amount;
+
+  if (amount >= 1000000000) {
+    parsed_amount = "0" + String((int)(amount / 1000000000.0)) + "B";
+  } else if (amount >= 1000000) {
+    parsed_amount = "0B";
+  } else if (amount >= 100000) {
+    parsed_amount = String((int)(amount / 100000.0)) + "M";
+  } else if (amount >= 10000) {
+    parsed_amount = "0M";
+  } else if (amount >= 1000) {
+    parsed_amount = String((int)(amount / 1000.0)) + "k";
+  } else if (amount >= 100) {
+    parsed_amount = "0k";
+  } else if (amount >= 10) {
+    parsed_amount = String(amount);
+  } else if (amount >= 1) {
+    parsed_amount = "0" + String(amount);
+  } else {
+    parsed_amount = "00";
   }
 
   return parsed_amount;
--- cyberpet/toolbox.h
+++ cyberpet/toolbox.h
@@ -3,7 +3,9 @@
 
 #include "Arduino.h"
 
-String format_number(unsigned long amount);
+String format_number4(unsigned long amount);
+String format_number3(unsigned long amount);
+String format_number2(unsigned long amount);
 int mod(int x, int y);
 
 #endif
